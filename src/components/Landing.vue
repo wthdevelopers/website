be50f9b91c67d939df05093e8ac50d858f702daf
TAYTS <taytzushieh@gmail.com>
2018-05-12 07:13:38 +0800

    import 'Quicksand 700' and 'Nunito Regular' font

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,4 +1,7 @@
 <template>
+  <div>
+
+  </div>
 </template>
 
 <script>
@@ -9,4 +12,6 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+@import url("https://fonts.googleapis.com/css?family=Quicksand:700");
+@import url("https://fonts.googleapis.com/css?family=Nunito");
 </style>
