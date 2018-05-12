0cecd96a6de2abf9c270bce16d84ae748ea09beb
Thanh <antoniedoan@gmail.com>
2018-05-12 19:56:37 +0800

    Add files via upload
    have some issues with faq.vue accordion, 
    sponsor and index just need some touch-ups

---
+++ src/components/faq.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="row" style="padding-top: 30px; text-align: center">
+    <h1 id="h1a">FAQs</h1>
+
+    <div id="faqs">
+<div id="faq">
+  <button id="query">Is this hackathon free?</button>
+  <div id="answer">
+    <p>Yes, this hackathon is free. All that we ask of you is for you to come down on the day itself!</p>
+    </div>
+  </div>
+
+  <div id="faq">
+    <button id="query">Who can join this hackathon?</button>
+    <div id="answer">
+      <p>This competition is open to anybody 13 years old and above as at the date of the competition. Anyone below 18 years old will require his/her parent’s consent to participate in the competition. Please print out the consent and indemnity form which can be found <a href="http://www.bit.ly/wthindemnityv1"> here </a>for your parent’s signature and bring it on the day of the competition.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+</template>
+
+<script>
+export default {
+  mounted() {
+    var acc = document.getElementsById("query");
+
+    for (var i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight){
+          panel.style.maxHeight = null;
+        } else {
+          panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+      });
+    }
+  }
+}
+</script>
+
+<style lang="css">
+@import url("https://fonts.googleapis.com/css?family=Nunito");
+
+#query {
+  background-color: #eee;
+  color: #444;
+  cursor: pointer;
+  padding: 18px;
+  width: 100%;
+  text-align: left;
+  border: none;
+  outline: none;
+  transition: 0.4s;
+}
+#answer {
+  padding: 0 18px;
+  background-color: white;
+  max-height: 0;
+  overflow: hidden;
+  transition: max-height 0.2s ease-out;
+}
+
+#h1a {
+  color: #ffa6c7;
+  font-family: 'Nunito', sans-serif;
+}
+
+</style>
---
+++ src/components/sponsor.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="sponsors">
+    <h1 id="h1b">Sponsors</h1>
+
+    <div class="platinum">
+      <h2>Platinum Tier</h2>
+      <div class="parent">
+        <div class="sponsor round shadow"></div>
+        <div class="sponsor round shadow"></div>
+        <div class="sponsor round shadow"></div>
+      </div>
+  </div>
+
+  <div class="silver">
+    <h2>Gold Tier</h2>
+    <div class="parent">
+      <div class="sponsor round shadow"></div>
+      <div class="sponsor round shadow"></div>
+      <div class="sponsor round shadow"></div>
+    </div>
+  </div>
+
+  <div class="platinum">
+  <h2>Silver Tier</h2>
+  <div class="parent">
+    <div class="sponsor round shadow"></div>
+    <div class="sponsor round shadow"></div>
+    <div class="sponsor round shadow"></div>
+  </div>
+  </div>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: 'sponsor'
+}
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css?family=Nunito");
+
+h1{
+  font-size: 300%;
+}
+
+#h1a {
+  color: #ffa6c7;
+}
+#h1b {
+  color: #a3d6f3;
+}
+
+.parent{
+  display: -webkit-box;
+  display: -moz-box;
+  display: -ms-flexbox;
+  display: -webkit-flex;
+  display: flex;
+  flex-wrap: no;
+
+  justify-content: space-around;
+
+}
+.sponsors{
+  font-family: 'Nunito', sans-serif;
+  text-align: center;
+}
+
+.sponsor{
+  height: 200px;
+  width: 200px;
+  margin-bottom: 40px;
+
+  background-color: #dbebff;
+}
+.round {
+  /* Safari 3-4, iOS 1-3.2, Android 1.6- */
+  -webkit-border-radius: 40px;
+
+  /* Firefox 1-3.6 */
+  -moz-border-radius: 40px;
+
+  /* Opera 10.5, IE 9, Safari 5, Chrome, Firefox 4, iOS 4, Android 2.1+ */
+  border-radius: 40px;
+}
+.shadow {
+  -moz-box-shadow:    0px 10px 0 0 #a3d6f3;
+  -webkit-box-shadow: 0px 10px 0 0 #a3d6f3;
+  box-shadow:         0px 10px 0 0 #a3d6f3;
+}
+</style>
